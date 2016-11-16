0759e7a57e5e0e2eb64cecff26a627f5f803fa5e
hbeckmann <hunterbeckmann@gmail.com>
2016-11-16 15:10:30 -0700

    Save song button

--- src/components/playlists/addSongDialog.js
+++ src/components/playlists/addSongDialog.js
@@ -8,31 +8,41 @@ var AddSongDialog = React.createClass({
     return(
       <div className="addSongDialog">
         <div className="dialogLine">
-          <label>Song Title</label><input type="text" id="addSongTitle" ref="addSongTitle" onKeyPress={this.saveSongDetails}></input>
+          <div className="closeButton">
+            X
+          </div>
         </div>
         <div className="dialogLine">
-          <label>URL</label><input type="text" id="addSongId" ref="addSongId" ></input>
+          <label>Song Title</label><input type="text" id="addSongTitle" ref="addSongTitle"></input>
         </div>
+        <div className="dialogLine">
+          <label>URL</label><input type="text" id="addSongId" ref="addSongId"  onKeyPress={this.saveSongDetailsEnter} ></input>
+        </div>
+        <button type="button" className="btn btn-primary saveButton" ref="saveSongButton" onClick={this.saveSongDetails} >Save</button>
       </div>
     );
   },
 
-  saveSongDetails: function(e) {
+  saveSongDetailsEnter: function(e) {
     if(e.key === "Enter") {
-      var title = this.refs.addSongTitle.value;
-      var id = this.refs.addSongId.value;
-
-      if(window.playlistId !== null) {
-        window.database.ref('/users/' + window.user.uid + '/playlists/' + window.playlistId + '/songs').push({title: title, id: id });
-      } else {
-        var newRef = window.database.ref('/users/' + window.user.uid + '/playlists/').push({playlistTitle: ""});
-        window.playlistId = newRef.key;
-        window.database.ref('/users/' + window.user.uid + '/playlists/' + window.playlistId + '/songs').push({title: title, id: id });
-      }
-
-      this.refs.addSongTitle.value = "";
-      this.refs.addSongId.value = "";
+      this.refs.saveSongButton.click();
     }
+  },
+
+  saveSongDetails: function() {
+    var title = this.refs.addSongTitle.value;
+    var id = this.refs.addSongId.value;
+
+    if(window.playlistId !== null) {
+      window.database.ref('/users/' + window.user.uid + '/playlists/' + window.playlistId + '/songs').push({title: title, id: id });
+    } else {
+      var newRef = window.database.ref('/users/' + window.user.uid + '/playlists/').push({playlistTitle: ""});
+      window.playlistId = newRef.key;
+      window.database.ref('/users/' + window.user.uid + '/playlists/' + window.playlistId + '/songs').push({title: title, id: id });
+    }
+
+    this.refs.addSongTitle.value = "";
+    this.refs.addSongId.value = "";
   }
 
 
